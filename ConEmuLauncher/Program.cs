f913aaad4ebd665f543e9381e48408b9019da34c
naminodarie <gengesa@gmail.com>
2019-03-03 17:31:14 +0900

    initial commit

---
+++ ConEmuLauncher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using static System.IO.Path;
+
+namespace ConEmuLauncher
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var selfDir = GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            string conemuPath = null;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                conemuPath = Combine(selfDir, "ConEmu64.exe");
+                if (!File.Exists(conemuPath))
+                    conemuPath = null;
+            }
+
+            if (conemuPath is null)
+            {
+                conemuPath = Combine(selfDir, "ConEmu.exe");
+                if (!File.Exists(conemuPath))
+                    conemuPath = null;
+            }
+
+            if (conemuPath is null) return;
+
+            System.Diagnostics.Process.Start(conemuPath, "-Single " + string.Join(" ", args));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ConEmuLauncher
-Wrapper of "ConEmu.exe -Single $args"
+
+## English
+
+Wrapper of "ConEmu.exe -Single $args" command.
+
+### How to Use
+
+1. copy emu.exe to ConEmu Directory, which has ConEmu.exe or ConEmu64.exe.
+1. Add ConEmu Directory to $PATH.
+
+You can launch "ConEmu -Single" from explorer or other app.
+
+
+## 日本語
+
+"ConEmu.exe -Single $args"コマンドのラッパーアプリです。
+
+### 使い方
+
+1. ConEmuのディレクトリ(ConEmu.exe or ConEmu64.exeがあるところ)にemu.exeをコピー
+1. ConEmuのディレクトリにパスをとおす
+
+エクスプローラーなどから"ConEmu -Single"で起動できるようになります。
